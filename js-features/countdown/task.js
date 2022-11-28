8ebee07796154a1a6f06e645b3e502bc57c763e8
Sergei Korchagin <gamestationbase@gmail.com>
2022-11-29 00:08:42 +0300

    correction

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -18,46 +18,88 @@
 
 // high level
 
-const timerCount = document.getElementById("timer");
-const [hh, mm, ss] = timerCount.textContent.split(':').map(Number);
+// const timerCount = document.getElementById("timer");
+// const [hh, mm, ss] = timerCount.textContent.split(':').map(Number);
 
 
 
 
 
-timer();
-let idInt = setInterval(timer, 1000);
+// timer();
+// let idInt = setInterval(timer, 1000);
 
-function timer() {
-    let now = new Date();
-    let targetTime = new Date(
-        now.getFullYear(),
-        now.getMonth(),
-        now.getDate(),
-        hh,
-        mm,
-        ss,
-    );
+// function timer() {
+//     let now = new Date();
+//     let targetTime = new Date(
+//         now.getFullYear(),
+//         now.getMonth(),
+//         now.getDate(),
+//         hh,
+//         mm,
+//         ss,
+//     );
 
-    let diffTimeSec = Math.ceil((targetTime.getTime() - now.getTime()) / 1000);
+//     let diffTimeSec = Math.ceil((targetTime.getTime() - now.getTime()) / 1000);
 
-    let hours = Math.floor(diffTimeSec / (60 * 60));
-    let diffAfterHours = diffTimeSec % (60 * 60);
-    let minutes = Math.floor((diffTimeSec % (60 * 60)) / 60);
-    let diffAfterMinutes = diffAfterHours % 60;
-    let seconds = diffAfterMinutes;
+//     let hours = Math.floor(diffTimeSec / (60 * 60));
+//     let diffAfterHours = diffTimeSec % (60 * 60);
+//     let minutes = Math.floor((diffTimeSec % (60 * 60)) / 60);
+//     let diffAfterMinutes = diffAfterHours % 60;
+//     let seconds = diffAfterMinutes;
 
-    timerCount.innerHTML = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds)
+//     timerCount.innerHTML = addZero(hours) + ":" + addZero(minutes) + ":" + addZero(seconds)
 
-    function addZero(num) {
-        if (num <= 9) {
-            num = `0` + num;
+//     function addZero(num) {
+//         if (num <= 9) {
+//             num = `0` + num;
+//         }
+
+//         return num;
+//     }
+
+//     if(timerCount.textContent === `00:00:00`) {
+//         clearInterval(idInt);
+//         }
+// }
+
+
+const output = document.getElementById('timer');
+const interval = 120;
+
+const start = new Date();
+
+function countdown(){
+    function addInsignZero(number, digitCount = 2) {
+        let string = `${number}`;
+        if (string.length < digitCount) {
+            string = '0' + addInsignZero(string, digitCount - 1);
         }
-
-        return num;
+        return string;
     }
 
-    if(timerCount.textContent === `00:00:00`) {
-        clearInterval(idInt);
-        }
+    let time = interval - Math.trunc((new Date() - start) / 1000);
+
+    if (time < 0) {
+        time = 0;
+    }
+
+    const seconds = time % 60;
+    const minutes = ((time - seconds) / 60) % 60;
+    const hours = Math.trunc(time / 3600);
+
+    output.textContent = addInsignZero(hours) + ':' + addInsignZero(minutes) + ':' + addInsignZero(seconds);
+
+    if (time > 0) {
+        setTimeout(countdown, 1000);
+    } else {
+        setTimeout(finish, 0);
+    }
 }
+
+function finish() {
+    alert('Вы победили в конкурсе!');
+}
+
+countdown(interval);
+
+
