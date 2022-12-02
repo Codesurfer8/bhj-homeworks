deb022313cff3ff440be36459c50bd2ae5356f11
Sergei Korchagin <gamestationbase@gmail.com>
2022-12-02 19:44:33 +0300

    add tasks

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,5 +1,6 @@
 const panelsButtons = document.querySelector(".book__control");
 const itemsButtons = Array.from(panelsButtons.children);
+const bookSize = document.getElementById("book");
 
 function closeSelection() {
     itemsButtons.forEach(
@@ -9,12 +10,27 @@ function closeSelection() {
     );
 };
 
+for (let i = 0; i < itemsButtons.length; i++) {
+    itemsButtons[i].addEventListener(
+        "click",
+        (e) => {
+            e.preventDefault();
+            closeSelection();
 
-for(let i = 0; i < itemsButtons.length; i++) {
-    itemsButtons[i].onclick = () => {
-        closeSelection();
-        itemsButtons[i].classList.add("font-size_active");
-        return;
-    };
+            itemsButtons[i].classList.add("font-size_active");
+
+            if (itemsButtons[i].classList.contains("font-size_small")) {
+                bookSize.classList.add("book_fs-small")
+                bookSize.classList.remove("book_fs-big")
+            } else if (itemsButtons[i].classList.contains("font-size_big")) {
+                bookSize.classList.add("book_fs-big")
+                bookSize.classList.remove("book_fs-small")
+            } else {
+                bookSize.classList.remove("book_fs-small")
+                bookSize.classList.remove("book_fs-big")
+            }
+        }
+    )
 };
+
 
